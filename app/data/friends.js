25f638df461d937436bd2c41c5da848bea88bad3
Chris Mikstas <chrismikstas@gmail.com>
2019-11-11 19:51:40 -0700

    completed post request on survey html page. started user data comparison on apiRoutes js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,9 @@
 var userArray =
 [
     {
-        "userName": "Ahmed",
-        "scores":
+        userName: "Satan",
+        photo:"https://www.snopes.com/tachyon/2018/01/Untitled-design-18-1.jpg?resize=865,452",
+        scores:
         [
             5,
             1,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,14 +3,26 @@ var userData = require("../data/friends");
 
 module.exports = function(app)
 {
-    app.get("/api/survey", function(req, res)
+    app.get("/api/friends", function(req, res)
     {
         res.json(userData);
     });
 
-    app.post("/api/survey", function(req, res)
+    app.post("/api/friends", function(req, res)
     {
+        for (let i = 0; i < userData.length; i++)
+        {
+            //console.log(userData);
+            let scoreDiff = 0;
+
+            for (let j = 0; j < userData[j].scores.length; j++)
+            {
+
+            }
+        }
+
         userData.push(req.body);
-        res.json(true);
+        //res.json(true);
+
     });
 };
